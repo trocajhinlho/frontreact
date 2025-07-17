09fc9a9784f00168c2391719e354915acd7bab41
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-17 16:04:31 -0300

    clic handler

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <string.h>
 #include "../dependencies/c2wasm.c"
@@ -5,23 +6,26 @@
 #include "pages/index.c"
 #include "pages/a.c"
 
-int main() {
-  ReactStart();
-  ReactRoot root = ReactDOMCreateRoot(ReactGetElementById("root")); 
-  ReactComponent app;
 
+ReactRoot root;
+
+void render_root(){
   char current_page[100] = {0};
   int size  = c2wasm_get_object_string_len_prop(c2wasm_window, "current_page");
   c2wams_object_memcpy_string(c2wasm_window,"current_page",0,current_page, size);
-
+  ReactComponent app;
   if(strcmp(current_page,"index")== 0){
     app = create_index_page();
   } 
   if(strcmp(current_page,"a")== 0){
     app = create_page_a();
   }
-
-
   ReactRootRender(root, app);
+}
+
+int main() {
+  ReactStart();
+  root = ReactDOMCreateRoot(ReactGetElementById("root")); 
+  render_root();
   return 0;
 }
--- src/pages/a.c
+++ src/pages/a.c
@@ -1,14 +1,32 @@
-
-#include <stdio.h>
 #include "../../dependencies/c2wasm.c"
 #include "../../dependencies/react.c"
 #include "../redirects.c"
-c2wasm_js_var create_style_a() {
+
+void render_root();
+
+int numero = 0;
+c2wasm_js_var increementar(c2wasm_js_var args) {
+    numero++;
+    render_root();
+    return c2wasm_undefined;
+}
+
+c2wasm_js_var create_style_do_numero() {
     return ReactCreateProps(
         "style", ReactCreateProps(
             "color", ReactCreateString("red")
         )
-        );
+    );
+}
+
+c2wasm_js_var create_a_numero() {
+    char numero_formatado [10] ={0};
+    sprintf(numero_formatado, "%d", numero);
+    return ReactCreateElement(
+                "h1", 
+                create_style_do_numero(),
+                ReactCreateString(numero_formatado)
+    );
 }
 
 ReactComponent create_page_a() {
@@ -16,9 +34,13 @@ ReactComponent create_page_a() {
             "div", 
             ReactNULL,
             ReactCreateElement(
-                "h1", 
-                create_style_a(),
-                ReactCreateString("#333")
-            )
+                "button",
+                ReactCreateProps(
+                    "onClick", ReactCreateClojure(increementar)
+                ),
+                ReactCreateString("incrementar")
+            ),
+            create_a_numero(),
+            create_a_numero()
     );
 }
